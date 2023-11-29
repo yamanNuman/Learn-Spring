e06527e1f1167429a3b84a873f7ab7be1f01c217
yamanNuman <yamannuman41@gmail.com>
2023-11-29 10:33:55 +0300

    content negotiation

--- restful-web-services/src/main/java/com/in28minutes/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.in28minutes.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	//@RequestMapping(path = "hello-world", method = RequestMethod.GET)
 	//return String
@@ -23,5 +33,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path="/hello-word-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Defult Message", locale);
 	}
 }
