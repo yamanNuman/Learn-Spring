890a19672b17fba40f7ba8242444d4db6ed2df93
yamanNuman <yamannuman41@gmail.com>
2023-12-09 19:03:48 +0300

    react useNavigate and authentication

--- front-end/todo-app/src/components/todo/TodoApp.jsx
+++ front-end/todo-app/src/components/todo/TodoApp.jsx
@@ -1,24 +1,83 @@
+import { useState } from "react";
+import "./TodoApp.css";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+
 export default function TodoApp() {
   return (
     <div className="TodoApp">
-      Todo Management Application
-      <LoginComponent />
-      {/* <WelcomeComponent /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginComponent />}></Route>
+          <Route path="/login" element={<LoginComponent />}></Route>
+          <Route path="/welcome" element={<WelcomeComponent />}></Route>
+          <Route path="*" element={<ErrorComponent />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 function LoginComponent() {
+  const [username, setUsername] = useState("in28minutes");
+  const [password, setPassword] = useState("");
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const navigate = useNavigate();
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit() {
+    if (username === "in28minutes" && password === "dummy") {
+      setShowSuccessMessage(true);
+      setShowErrorMessage(false);
+      navigate("/welcome");
+    } else {
+      setShowSuccessMessage(false);
+      setShowErrorMessage(true);
+      navigate("/login");
+    }
+  }
+
   return (
     <div className="Login">
+      <h1>Time to Login !</h1>
+      {showSuccessMessage && (
+        <div className="errorMessage">Authentication Succesfully.</div>
+      )}
+      {showErrorMessage && (
+        <div className="errorMessage">
+          Authentication Failed. Please check your credentials.
+        </div>
+      )}
       <div className="LoginForm">
         <div>
           <label>Username </label>
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
         </div>
         <div>
           <label>Password </label>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div>
+          <button type="button" name="login" onClick={handleSubmit}>
+            Login
+          </button>
         </div>
       </div>
     </div>
@@ -26,5 +85,21 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
-  return <div className="Welcome">Welcome Component</div>;
+  return (
+    <div className="WelcomeComponent">
+      <h1>Welcome in28minutes</h1>
+      Welcome Component
+    </div>
+  );
+}
+
+function ErrorComponent() {
+  return (
+    <div className="ErrorComponent">
+      <h1>We are working really hard!</h1>
+      <div>
+        Apologies for the 404. Reach out to our team at PIRILTI SOFTWARE.
+      </div>
+    </div>
+  );
 }
