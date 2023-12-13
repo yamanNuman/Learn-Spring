d934d0f38733c33f3d4ad566152ee470e0e16fd0
yamanNuman <yamannuman41@gmail.com>
2023-12-13 18:23:28 +0300

    spring+react

--- front-end/todo-app/src/components/todo/ListTodosComponent.jsx
+++ front-end/todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { retrieveAllTodosForUsername } from "./api/TodoApiService";
+import { useEffect } from "react";
+
 function ListTodosComponent() {
   const today = new Date();
   const targetDate = new Date(
@@ -5,16 +9,29 @@ function ListTodosComponent() {
     today.getMonth(),
     today.getDay()
   );
-  const todos = [
-    { id: 1, description: "Learn AWS", done: false, targetDate: targetDate },
-    { id: 2, description: "Learn Azure", done: false, targetDate: targetDate },
-    {
-      id: 3,
-      description: "Learn Network",
-      done: false,
-      targetDate: targetDate,
-    },
-  ];
+  const [todos, setTodos] = useState([]);
+  // const todos = [
+  // { id: 1, description: "Learn AWS", done: false, targetDate: targetDate },
+  // { id: 2, description: "Learn Azure", done: false, targetDate: targetDate },
+  // {
+  //   id: 3,
+  //   description: "Learn Network",
+  //   done: false,
+  //   targetDate: targetDate,
+  // },
+  // ];
+
+  useEffect(() => refreshTodos, []);
+
+  function refreshTodos() {
+    retrieveAllTodosForUsername("in28minutes")
+      .then((response) => {
+        console.log(response.data);
+        setTodos(response.data);
+      })
+      .catch((error) => console.log(error));
+  }
+
   return (
     <div className="container">
       <h1>Things you want to do</h1>
@@ -34,7 +51,7 @@ function ListTodosComponent() {
                 <td>{todo.id}</td>
                 <td>{todo.description}</td>
                 <td>{todo.done.toString()}</td>
-                <td>{todo.targetDate.toDateString()}</td>
+                <td>{todo.targetDate.toString()}</td>
               </tr>
             ))}
           </tbody>
--- front-end/todo-app/src/components/todo/WelcomeComponent.jsx
+++ front-end/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,13 +1,50 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import {
+  retrieveHelloWorldBean,
+  retrieveHelloWorldBeanPathVariable,
+} from "./api/HelloWorldApiService";
 
 function WelcomeComponent() {
   const { username } = useParams();
+  const [message, setMessage] = useState();
+
+  function callHelloWorldRestApi() {
+    // axios
+    //   .get("http://localhost:8080/hello-world")
+    //   .then((response) => successfulResponse(response))
+    //   .catch((error) => errorResponse(error))
+    //   .finally(() => console.log("cleanup"));
+
+    // retrieveHelloWorldBean()
+    //   .then((response) => successfulResponse(response))
+    //   .catch((error) => errorResponse(error))
+    //   .finally(() => console.log("cleanup"));
+
+    retrieveHelloWorldBeanPathVariable("Javi")
+      .then((response) => successfulResponse(response))
+      .catch((error) => errorResponse(error))
+      .finally(() => console.log());
+  }
+
+  function successfulResponse(response) {
+    setMessage(response.data.message);
+  }
+  function errorResponse(error) {
+    console.log(error);
+  }
   return (
     <div className="WelcomeComponent">
       <h1>Welcome {username}</h1>
       <div>
         Manage your todos - <Link to="/todos">Go here</Link>
       </div>
+      <div>
+        <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+          Call Hello World REST API
+        </button>
+      </div>
+      <div className="text-info">{message}</div>
     </div>
   );
 }
